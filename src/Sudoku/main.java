631da0a79d4b1ac3622a4d1c087861005c05434c
Keybowsky <chabowskiadrian1@gmail.com>
2019-05-15 19:05:22 +0200

    update

--- src/Sudoku/main.java
+++ src/Sudoku/main.java
@@ -6,13 +6,6 @@ public class main {
 
     public static void main(String[] args) {
 
-        
-        
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                toolBar.createAndShowGUI();
-            }
-        });
         /*
 
         int tablica [][]={
